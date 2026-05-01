d2699fc4a59f607dc7dfce03a0fdcdfa7d3cdb35
Fernando Arispe <fernandoarispe92@gmail.com>
2026-05-01 17:10:22 -0300

    Inicio de proyecto

---
+++ Gestion-Canchas/BLL/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+using DAL;
+using Security;
+
+namespace BLL
+{
+    public class Class1
+    {
+    }
+}
