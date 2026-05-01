75aa002e98e7d670c74d82a900690df27875d8e2
Fernando Arispe <fernandoarispe92@gmail.com>
2026-05-01 17:46:31 -0300

    Conexion a la base de datos

---
+++ Gestion-Canchas/DAL/DAO.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using BE;
+using Security;
+
+namespace DAL
+{
+    public class DAO
+    {
+        static SqlConnection mConexion = new SqlConnection(ConfigurationManager.ConnectionStrings["cadena"].ConnectionString); // Base ""
+
+        public static DataSet EjecutarDataSet(string pCommand)
+        {
+            try
+            {
+                SqlDataAdapter mDa = new SqlDataAdapter(pCommand, mConexion);
+                DataSet mDs = new DataSet();
+                mDa.Fill(mDs);
+                return mDs;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (mConexion.State != ConnectionState.Closed)
+                {
+                    mConexion.Close();
+                }
+            }
+        }
+
+        public static int EjecutarNonQuery(string pCommand)
+        {
+            try
+            {
+                SqlCommand mCom = new SqlCommand(pCommand, mConexion);
+                mConexion.Open();
+                return mCom.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (mConexion.State != ConnectionState.Closed)
+                {
+                    mConexion.Close();
+                }
+            }
+        }
+    }
+}
